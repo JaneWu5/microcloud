63c0f31239b1f7daef226a6600aeb67649659611
wuzheng <jenniferwzz@qq.com>
2018-06-06 01:00:31 +0800

    竟然加了个配置瞬间好了

--- microcloud-provider-dept-8001/src/main/java/com/wuzheng/microcloud/config/SecurityConfig.java
+++ microcloud-provider-dept-8001/src/main/java/com/wuzheng/microcloud/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.wuzheng.microcloud.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -12,9 +13,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  * @Description: SpringSecurity配置文件
  */
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
         http.csrf().disable();
     }
 }
