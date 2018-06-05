e94fef914db20564ca3f26106c1f369f13d2a685
wuzheng <wuzheng@guazi.com>
2018-06-05 20:30:55 +0800

    加入springSecurity配置，Post过不去

--- microcloud-consumer-80/src/main/java/com/wuzheng/microcloud/config/RestConfig.java
+++ microcloud-consumer-80/src/main/java/com/wuzheng/microcloud/config/RestConfig.java
@@ -2,7 +2,11 @@ package com.wuzheng.microcloud.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
+import java.util.Base64;
 
 /**
  * @ClassName: RestConfig
@@ -14,7 +18,17 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestConfig {
     @Bean
-    public RestTemplate getRestTemplate(){
+    public HttpHeaders getHttpHeaders() { //Http头信息配置
+        HttpHeaders headers = new HttpHeaders();
+        String auth = "wuzheng:hello";
+        byte[] encodedAuth = Base64.getEncoder().encode(auth.getBytes(Charset.forName("US-ASCII")));
+        String authHeader = "Basic " + new String(encodedAuth);
+        headers.set("Authorization", authHeader);
+        return headers;
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
 }
--- microcloud-consumer-80/src/main/java/com/wuzheng/microcloud/controller/DeptController.java
+++ microcloud-consumer-80/src/main/java/com/wuzheng/microcloud/controller/DeptController.java
@@ -1,6 +1,10 @@
 package com.wuzheng.microcloud.controller;
 
 import com.wuzheng.microcloud.entity.Dept;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,25 +25,33 @@ import java.util.List;
 public class DeptController {
     public static final String DEPT_GET_URL = "http://localhost:8001/dept/get/";
     public static final String DEPT_LIST_URL = "http://localhost:8001/dept/list";
-    public static final String DEPT_ADD_URL = "http://localhost:8001/dept/add";
+    public static final String   DEPT_ADD_URL = "http://localhost:8001/dept/add";
 
     @Resource
     private RestTemplate restTemplate;
+    @Resource
+    private HttpHeaders headers;
 
     @GetMapping("/get")
     public Dept getDept(Long id) {
-        Dept dept = restTemplate.getForObject(DEPT_GET_URL + id, Dept.class);
+//        Dept dept = restTemplate.getForObject(DEPT_GET_URL + id, Dept.class);
+        Dept dept = restTemplate.exchange(DEPT_GET_URL + id, HttpMethod.GET,
+                new HttpEntity<>(headers), Dept.class).getBody();
         return dept;
     }
 
     @GetMapping("/list")
     public List<Dept> listDept() {
-        List<Dept> deptList = restTemplate.getForObject(DEPT_LIST_URL, List.class);
+//        List<Dept> deptList = restTemplate.getForObject(DEPT_LIST_URL, List.class);
+        List<Dept> deptList = restTemplate.exchange(DEPT_LIST_URL, HttpMethod.GET,
+                new HttpEntity<>(headers), List.class).getBody();
         return deptList;
     }
 
     @GetMapping("/add")
     public Boolean addDept(Dept dept) {
-        return restTemplate.postForObject(DEPT_ADD_URL, dept, Boolean.class);
+//        return restTemplate.postForObject(DEPT_ADD_URL, dept, Boolean.class);
+        return restTemplate.exchange(DEPT_ADD_URL, HttpMethod.POST,
+                new HttpEntity<>(dept, headers), Boolean.class).getBody();
     }
 }
